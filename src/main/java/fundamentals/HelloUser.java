0af766316f169ca67ff05a86f06c44810f4eb1c0
Ivan Fomkin <ivan-fomkin@yandex.ru>
2021-01-03 01:23:40 +0300

    Add two first tasks

--- src/main/java/fundamentals/HelloUser.java
+++ src/main/java/fundamentals/HelloUser.java
@@ -1,4 +1,11 @@
 package fundamentals;
 
 public class HelloUser {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0]);
+        } else {
+            System.out.println("Can't get args!");
+        }
+    }
 }
