4a13f6ef2aa2ef799a446dc040278e00ef3489eb
Ivan Fomkin <ivan-fomkin@yandex.ru>
2021-01-03 02:11:19 +0300

    Add last task

---
+++ src/main/java/fundamentals/GetMonth.java
@@ -0,0 +1,30 @@
+package fundamentals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class GetMonth {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int enteredNumber = 0;
+        System.out.print("Enter number: ");
+        try {
+            enteredNumber = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (enteredNumber > 0 && enteredNumber <= 12) {
+            printMonthNameByNumber(enteredNumber);
+        } else {
+            System.out.println("Incorrect number");
+        }
+    }
+
+    public static void printMonthNameByNumber(int number) {
+        System.out.println(Month.of(number).getDisplayName(TextStyle.FULL_STANDALONE, new Locale("ru")));
+    }
+}
